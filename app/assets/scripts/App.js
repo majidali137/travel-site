774780f08128753fd33d98afb0f1c2c03907cf12
Majid <majidaliwp69@gmail.com>
2022-12-15 18:45:34 +0500

    half complete mobile menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,6 @@
 import '../styles/styles.css'
+import mobileMenu from './modules/mobileMenu'
+let mobileMenus = new mobileMenu ();
 //alert("Hello! Welcome To my first Office Project")
 if (module.hot) {
     module.hot.accept()
