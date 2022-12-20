a869087c58a2dfa0f556523bb7f221949a1db00d
Majid <majidaliwp69@gmail.com>
2022-12-20 19:32:38 +0500

    add build

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 
 const postCSSPlugins = [
@@ -8,32 +9,10 @@ const postCSSPlugins = [
     require('postcss-hexrgba'),
     require('autoprefixer')
   ]
-module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-      static:{
-        before:function(app, server) {
-            server._watch('./app/**/*.html')
-        },
 
-      },
-        static: {
-            
-          directory: path.join(__dirname, "app")
-        },
-    
-        hot: true,
-        port: 3000,// default 8000
-        host: '0.0.0.0'
-      },
-
-
-   mode: 'development',
-   module: {
+let config = {
+  entry: './app/assets/scripts/App.js',
+  module: {
     rules: [
         {
             test: /\.css$/i,
@@ -43,3 +22,38 @@ module.exports = {
     ]
    }
 }
+
+if(currentTask == 'dev') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+}
+config.devServer = {
+  static:{
+    before:function(app, server) {
+        server._watch('./app/**/*.html')
+    },
+
+  },
+    static: {
+        
+      directory: path.join(__dirname, "app")
+    },
+
+    hot: true,
+    port: 3000,// default 8000
+    host: '0.0.0.0'
+  }
+config.mode = 'development'
+
+}
+
+if(currentTask == 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+}
+  config.mode = 'production'
+}
+
+module.exports = config
